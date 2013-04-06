19bbacb6811af3a46a24ebac51d64cdb24caf99a
Keryn Knight <keryn@kerynknight.com>
2013-04-06 13:36:59 +0100

    slowly working up to only the most targeted ajax requests, so that dropping something back to where it was doesn't do an unnecessary change.

--- editregions/static/editregions/js/dragging.js
+++ editregions/static/editregions/js/dragging.js
@@ -3,8 +3,16 @@
     var handle = 'div.drag_handle';
     var sortable_targets = '.results';
 
+    var start_region;
+    var start_position;
+    var target_region;
+    var target_position;
+
     var maybe_move_region = function(e, ui){
         $(this).find("tbody").append(ui.item);
+        // rebinds the region to the parent scope
+        target_region = $(this).parent().attr('data-region');
+        target_position = ui.item.index();
     };
 
     var table_helper = function(e, tr) {
@@ -16,8 +24,8 @@
         return $helper;
     };
 
-    var update_remote_object = function(obj, requested_position, region) {
-        alert('making ' + obj + ' be at position ' + requested_position + ' in "' + region + '"');
+    var update_remote_object = function(sortable, event, ui, id, position, region) {
+        alert('making ' + id + ' be at position ' + position + ' in "' + region + '"');
     };
 
     var finish_changelist_changes = function(e, ui) {
@@ -28,17 +36,22 @@
             $(this).removeClass('row1 row2').addClass('row'+ target_class);
             $('td:nth-child(1)', this).html(i+1);
         });
-        var new_position = Math.min(1, ui.item.prevUntil().length + 1);
+
         var obj_id = ui.item.find(handle).eq(0).attr('data-pk');
-        var target_region = $(this).parent().attr('data-region');
-        if (obj_id === void 0 || target_region === void 0) {
-            alert('error grabbing required data');
-        } else {
-            update_remote_object(obj_id, new_position, target_region);
-        };
+        // in case of undefined after `maybe_move_region`, test again here.
+        target_region = target_region || $(this).parent().attr('data-region');
+        target_position = target_position || ui.item.index();
+
+        if (target_position !== start_position || target_region !== start_region) {
+            update_remote_object(this, e, ui, obj_id, target_position, target_region);
+        }
     };
 
     var start_changelist_changes = function(e, ui) {
+        // into parent scope.
+        start_position = ui.item.index();
+        start_region = $(this).parent().attr('data-region');
+
         var html = ui.item.html();
         ui.placeholder.html(html);
     };
