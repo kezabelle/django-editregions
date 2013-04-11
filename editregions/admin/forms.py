0717c9e925e4c26d816d05a112941bd1c62d6abd
Keryn Knight <keryn@kerynknight.com>
2013-04-11 21:22:48 +0100

    document and comment and see if stuff breaks.

--- editregions/admin/forms.py
+++ editregions/admin/forms.py
@@ -10,10 +10,18 @@ from editregions.utils.data import queryset_to_attr_map
 
 
 class EditRegionInlineForm(object):
+    """
+    Used by EditRegionInlineForm
+    """
     media = Media()
 
 
 class EditRegionInlineFormSet(object):
+    """
+    A minimal representation of a FormSet as called by the Django inline
+    admin code. The most importand bit is our media definition, everything else
+    is literally just to appear correct.
+    """
     initial_forms = []
     extra_forms = []
     media = shared_media
@@ -40,41 +48,41 @@ class EditRegionInlineFormSet(object):
 
     def save(self, *args, **kwargs):
         return True
-
-
-class ReorderChunksForm(Form):
-    model = EditRegionChunk
-    items = TypedMultipleChoiceField(coerce=int, choices=())
-
-    def __init__(self, content_type, obj_id, *args, **kwargs):
-        super(ReorderChunksForm, self).__init__(*args, **kwargs)
-        self.fields['items'].choices = ((x.pk, unicode(x.pk))
-            for x in self.model.objects.filter(content_type=content_type,
-                content_id=obj_id).only('id'))
-
-        # Fix the stupid incoming data format.
-        incoming_data = self.data.copy()
-        # jQuery uses an underscore to avoid browser caching.
-        uncache = u'_'
-        if uncache in incoming_data:
-            del incoming_data[uncache]
-        # this should probably be in validation.
-        assert len(incoming_data) == 1, "Unexpected data received"
-        data_values = incoming_data.popitem()[1]
-        nondigits = re.compile(r'[^\d]+')
-        data_for_binding = ['items=%d' % int(nondigits.sub('', x))
-                            for x in data_values if x]
-        self.data = QueryDict('&'.join(data_for_binding), mutable=False)
-
-    def save(self):
-        items = self.cleaned_data['items']
-        chunks_affected = self.model.objects.filter(pk__in=items)
-        chunk_map = queryset_to_attr_map(chunks_affected, 'pk')
-        for new_position, chunk in enumerate(self.cleaned_data['items']):
-            real_obj = chunk_map[chunk]
-            real_obj.position = new_position
-            real_obj.save()
-        return True
+#
+#
+# class ReorderChunksForm(Form):
+#     model = EditRegionChunk
+#     items = TypedMultipleChoiceField(coerce=int, choices=())
+#
+#     def __init__(self, content_type, obj_id, *args, **kwargs):
+#         super(ReorderChunksForm, self).__init__(*args, **kwargs)
+#         self.fields['items'].choices = ((x.pk, unicode(x.pk))
+#             for x in self.model.objects.filter(content_type=content_type,
+#                 content_id=obj_id).only('id'))
+#
+#         # Fix the stupid incoming data format.
+#         incoming_data = self.data.copy()
+#         # jQuery uses an underscore to avoid browser caching.
+#         uncache = u'_'
+#         if uncache in incoming_data:
+#             del incoming_data[uncache]
+#         # this should probably be in validation.
+#         assert len(incoming_data) == 1, "Unexpected data received"
+#         data_values = incoming_data.popitem()[1]
+#         nondigits = re.compile(r'[^\d]+')
+#         data_for_binding = ['items=%d' % int(nondigits.sub('', x))
+#                             for x in data_values if x]
+#         self.data = QueryDict('&'.join(data_for_binding), mutable=False)
+#
+#     def save(self):
+#         items = self.cleaned_data['items']
+#         chunks_affected = self.model.objects.filter(pk__in=items)
+#         chunk_map = queryset_to_attr_map(chunks_affected, 'pk')
+#         for new_position, chunk in enumerate(self.cleaned_data['items']):
+#             real_obj = chunk_map[chunk]
+#             real_obj.position = new_position
+#             real_obj.save()
+#         return True
 
 
 class MovementForm(Form):
