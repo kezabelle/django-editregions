ced6f160badd099f6b2768b96a0ec1ead98a6151
Keryn Knight <keryn@kerynknight.com>
2013-04-11 21:00:30 +0100

    move to our own version of the {% result_list %} template tag, just to change the template. The lack of blocks saddens me though.

---
+++ editregions/templatetags/editregion_result_list.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from django.template import Library
+from django.contrib.admin.templatetags.admin_list import (result_headers,
+                                                          result_hidden_fields,
+                                                          results)
+
+register = Library()
+
+@register.inclusion_tag("admin/editregions/change_list_results.html")
+def editregion_result_list(cl):
+    """
+    This literally only exists to change the template and fix it properly.
+    """
+    headers = list(result_headers(cl))
+    num_sorted_fields = 0
+    for h in headers:
+        if h['sortable'] and h['sorted']:
+            num_sorted_fields += 1
+    return {'cl': cl,
+            'result_hidden_fields': list(result_hidden_fields(cl)),
+            'result_headers': headers,
+            'num_sorted_fields': num_sorted_fields,
+            'results': list(results(cl))}
