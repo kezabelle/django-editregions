dc77722d65a73575ec8203a16cd3ee303a9b6cf3
Keryn Knight <keryn@kerynknight.com>
2013-04-27 12:53:03 +0100

    get_model_class/get_content_type/get_modeladmin helper methods.

--- editregions/utils/data.py
+++ editregions/utils/data.py
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
+from django.contrib.contenttypes.models import ContentType
+from adminlinks.templatetags.utils import get_admin_site
+from django.db.models import get_model
+
 
 def queryset_to_attr_map(queryset, attr):
     output = {}
@@ -7,6 +11,7 @@ def queryset_to_attr_map(queryset, attr):
         output[result] = obj
     return output
 
+
 def convert_context_to_dict(context):
     dicts = context.dicts
     out = {}
@@ -14,3 +19,35 @@ def convert_context_to_dict(context):
         for key, value in d.items():
             out[key] = value
     return out
+
+
+def get_model_class(obj):
+    """
+    compatibility layer over the fact get_model is internal and could go away
+    in the future, which would require us switch over to using contenttypes.
+
+    :param obj: a model instance
+    :return: an installed model class, or None.
+    """
+    if not hasattr(obj, '_meta'):
+        raise AttributeError('Not a model object')
+
+    app, model = obj._meta.app_label, obj._meta.module_name
+    model = get_model(app_label=app, model_name=model, only_installed=True)
+    return model
+
+
+def get_content_type(input):
+    if hasattr(input, '_meta'):
+        return ContentType.objects.get_for_model(input)
+    else:
+        return ContentType.objects.get_for_id(input)
+
+
+def get_modeladmin(obj, admin_namespace='admin'):
+    """
+    convienience function around finding the modeladmin we want.
+    Allows us to provide a class or instance and get back the modeladmin.
+    """
+    model = get_model_class(obj)
+    return get_admin_site(admin_namespace)._registry[model]
