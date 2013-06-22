06e85e27431f8605a644fa1205728044dace7530
Keryn Knight <keryn@kerynknight.com>
2013-06-22 13:32:01 +0100

    fixing the toolbar link now we've changed urlconf

--- editregions/templatetags/adminlinks_editregion.py
+++ editregions/templatetags/adminlinks_editregion.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 import logging
-from adminlinks.templatetags.adminlinks_buttons import BaseAdminLink
+from adminlinks.templatetags.adminlinks_buttons import BaseAdminLink, _changelist_popup_qs
 from django import template
 from django.http import QueryDict
 from editregions.models import EditRegionChunk
@@ -27,46 +27,33 @@ class EditRegionToolbar(BaseAdminLink, InclusionTag):
 
     options = Options(
         BaseAdminLink.base_options[0],  # obj
-        StringArgument('region_name', required=True, resolve=True),
+        # StringArgument('region_name', required=True, resolve=True),
         StringArgument('admin_site', required=False, default='admin'),
-        Argument('querystring', required=False, default=''),
+        Argument('querystring', required=False, default=_changelist_popup_qs()),
     )
 
-    def get_context(self, context, obj, region_name, admin_site, querystring):
+    def get_link_context(self, context, obj, admin_site, querystring):
         """
         ..todo:: document
 
         Always returns the existing context.
         """
-        validate_region_name(region_name)
-
-        if not self.is_valid(context, obj):
-            return context
-
+        # validate_region_name(region_name)
         site = get_admin_site(admin_site)
         if site is None:
             logger.debug('Invalid admin site')
-            return context
-
+            return {}
+        content_type = get_content_type(obj)
         link = _add_custom_link_to_context(admin_site, context['request'],
                                            EditRegionChunk._meta, 'change',
-                                           'changelist', None, query=querystring)
-        # just using the existance of link as a guard to ensure permissions
-        # checked out and the admin is loaded.
-        if link['link']:
-            querystring = QueryDict(querystring)
-            wrapped = AdminChunkWrapper(opts=EditRegionChunk._meta,
-                                        namespace=site.name,
-                                        content_id=obj.pk,
-                                        content_type=get_content_type(obj),
-                                        region=region_name)
-            wrapped.querydict.update(querystring)
-            # replace the default values with our own, better ones :\
-            templates = obj.get_region_groups()
-            template = get_first_valid_template(templates)
-            link.update(link=wrapped.get_manage_url(),
-                        verbose_name=get_pretty_region_name(template, region_name))
-        context.update(link)
-        return context
+                                           'changelist', [content_type.pk, obj.pk],
+                                           query=querystring)
+
+        if link['verbose_name']:
+            logger.debug('link created successfully, swapping out the '
+                         '`verbose_name` available to the context')
+            link['verbose_name'] = EditRegionChunk._meta.verbose_name_plural
+            link['obj_name'] = obj._meta.verbose_name
+        return link
 register.tag(name='render_adminlinks_editregion',
              compile_function=EditRegionToolbar)
