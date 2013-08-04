1e714672e8f425c6432e916ca3750fc2b989efcf
Keryn Knight <keryn@kerynknight.com>
2013-08-04 13:41:42 +0100

    rendered results can now be an used as an `as tag` {% editreigon "region_name" object_variable as OUTNAME %} {% for x in OUTNAME %}<div>{{ x }}</div>{% endfor %}

--- editregions/templatetags/editregion.py
+++ editregions/templatetags/editregion.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 import logging
+from classytags.helpers import AsTag
 from django import template
 from django.conf import settings
 from django.contrib.contenttypes.models import ContentType
-from classytags.core import Tag, Options
+from classytags.core import Options
 from classytags.arguments import Argument, StringArgument
 from django.core.cache import cache, DEFAULT_CACHE_ALIAS
 from django.core.exceptions import ImproperlyConfigured
@@ -19,7 +20,7 @@ register = template.Library()
 logger = logging.getLogger(__name__)
 
 
-class EditRegionTag(Tag):
+class EditRegionTag(AsTag):
     """
     Output the contents of a region in a region group::
 
@@ -40,6 +41,7 @@ class EditRegionTag(Tag):
     options = Options(
         StringArgument('name', required=True, resolve=True),
         Argument('content_object', required=True, default=None, resolve=True),
+        'as', Argument('output_var', required=False, default=None, resolve=False)
     )
 
     def render_tag(self, context, name, content_object, **kwargs):
@@ -53,18 +55,30 @@ class EditRegionTag(Tag):
                 'tagname': _tag_name,
                 'region': name,
             }
+            logger.error(error)
             if settings.DEBUG:
                 raise ImproperlyConfigured(error)
-            else:
-                logger.error(error)
 
+        #: this is basically from the core :class:`~classytags.core.Tag`
+        #: implementation but changed to allow us to have different output
+        #: so that using it as an AS tag returns a *list* of chunks, while
+        #: doing it as a normal tag just outputs a string.
+        varname = kwargs.pop(self.varname_name)
+        if varname:
+            context[varname] = self.get_value(context, name, content_object,
+                                              **kwargs)
+            return ''
+        else:
+            return self.get_tag(context, name, content_object, **kwargs)
+
+    def get_content_type(self, content_object):
         try:
-            content_type = get_content_type(content_object)
+            return get_content_type(content_object)
         except ContentType.DoesNotExist:
             # the model doesn't exist in the content types table. I don'
             #  know why.
             logger.error('content object does not exist')
-            return u''
+            return None
         except AttributeError:
             # we didn't get a proper django model, but something has definitely
             # been passed in, because the earlier None sentinel didn't catch it.
@@ -76,7 +90,12 @@ class EditRegionTag(Tag):
                 raise ImproperlyConfigured(error)
             else:
                 logger.error(error)
-            return u''
+            return None
+
+    def get_value(self, context, name, content_object, **kwargs):
+        content_type = self.get_content_type(content_object)
+        if content_type is None:
+            return ()
 
         results = get_chunks_for_region(content_id=content_object.pk,
                                         content_type=content_type, region=name)
@@ -94,8 +113,13 @@ class EditRegionTag(Tag):
             logger.debug("key {key} was not found in the '{cache}' backend, "
                          "so we're polling the DB for chunks".format(key=KEY,
                          cache=DEFAULT_CACHE_ALIAS))
-            cached_val = u'\n'.join(render_all_chunks(template, context, name,
-                                                      results))
-            cache.set(KEY, cached_val)
+            cached_val = render_all_chunks(template, context, name, results)
         return cached_val
+
+    def get_tag(self, context, name, content_object, **kwargs):
+        results = self.get_value(context, name, content_object, **kwargs)
+        if results is None:
+            return u''
+
+        return u'\n'.join(results)
 register.tag('editregion', EditRegionTag)
