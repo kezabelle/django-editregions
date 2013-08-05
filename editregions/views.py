56327efab4d120b7150d884edc3845c0a571e07e
Keryn Knight <keryn@kerynknight.com>
2013-08-05 16:46:05 +0100

    the EditRegionResponseMixin, which allows for redirects if FormSuccess is thrown, should not need to inherit from a core GCBV. Nor should it *need* the messages framework.

--- editregions/views.py
+++ editregions/views.py
@@ -2,9 +2,9 @@
 import logging
 from django.contrib import messages
 from django.shortcuts import redirect
-from django.views.generic.base import TemplateResponseMixin
 
 logger = logging.getLogger(__name__)
+
 
 class FormSuccess(Exception):
     def __init__(self, location, msg=None, permanent=False):
@@ -13,13 +13,11 @@ class FormSuccess(Exception):
         self.permanent = permanent
 
 
-class EditRegionResponseMixin(TemplateResponseMixin):
+class EditRegionResponseMixin(object):
     """
-    A mixin which implements the standard
-    :class:`~django.views.generic.base.TemplateResponseMixin` features, with the
-    additional feature that any view (or template, or template tag) which
-    raises a :exc:`~editregions.views.FormSuccess` exception will *not*
-    error, and will instead redirect to a given ``location``, also
+    A mixin which allows for any view (or template, or template tag) which
+    raises a :exc:`~editregions.views.FormSuccess` exception to avoid showing
+    an error, and will instead redirect to a given ``location``, also
     setting a flash message.
     """
     def render_to_response(self, context, **response_kwargs):
@@ -33,7 +31,7 @@ class EditRegionResponseMixin(TemplateResponseMixin):
                 context, **response_kwargs)
         except FormSuccess as e:
             if e.msg is not None:
-                messages.success(self.request, e.msg)
+                messages.success(self.request, e.msg, fail_silently=True)
             logger.debug('%(path)s raised `FormSuccess`, redirecting to '
                          'new endpoint %(new_path)s' % {
                              'path': self.request.path,
