3d5d5d496e59ba5624c93e6bba213e8ebcf9800e
Keryn Knight <keryn@kerynknight.com>
2013-11-09 15:28:11 +0000

    make part of the "public" API

--- editregions/admin/__init__.py
+++ editregions/admin/__init__.py
@@ -2,4 +2,5 @@
 from django.contrib import admin
 from editregions.models import EditRegionChunk
 from editregions.admin.modeladmins import EditRegionAdmin
+from editregions.admin.inlines import EditRegionInline
 admin.site.register(EditRegionChunk, EditRegionAdmin)
