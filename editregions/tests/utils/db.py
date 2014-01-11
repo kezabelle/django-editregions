a5a7431cb56c38c13ef30715216a8b21cae637f4
Keryn Knight <keryn@kerynknight.com>
2014-01-11 19:34:56 +0000

    test `get_chunks_in_region_count`

--- editregions/tests/utils/db.py
+++ editregions/tests/utils/db.py
@@ -5,7 +5,7 @@ from django.utils.unittest.case import TestCase
 from django.test import TestCase as DjangoTestCase
 from editregions.models import EditRegionChunk
 from editregions.utils.data import get_content_type
-from editregions.utils.db import get_maximum_pk, set_new_position
+from editregions.utils.db import get_maximum_pk, set_new_position, get_chunks_in_region_count
 
 
 class GetMaximumPKTestCase(DjangoTestCase):
@@ -36,8 +36,34 @@ class SetNewPositionTestCase(DjangoTestCase):
 
         last = existing[-1]
         self.assertEqual(last, 10)
-        
+
         set_new_position(EditRegionChunk, last, 0)
         existing = list(EditRegionChunk.objects.all().order_by('position')
                         .values_list('pk', flat=True))
         self.assertEqual(existing, [1, 10, 2, 3, 4, 5, 6, 7, 8, 9])
+
+
+class GetChunksInRegionCountTestCase(DjangoTestCase):
+    def setUp(self):
+        sample_user, created = User.objects.get_or_create()
+        user_ct = get_content_type(sample_user)
+        self.user = sample_user
+        self.content_type = user_ct
+
+    def test_count(self):
+        for x in range(1, 10):
+            obj = EditRegionChunk(region='test', position=x,
+                                  content_id=self.user.pk,
+                                  content_type=self.content_type)
+            obj.full_clean()
+            obj.save()
+        count = get_chunks_in_region_count(EditRegionChunk,
+                                           content_type=self.content_type,
+                                           obj_id=self.user.pk, region='test')
+        self.assertEqual(9, count)
+
+    def test_nothing(self):
+        count = get_chunks_in_region_count(EditRegionChunk,
+                                           content_type=self.content_type,
+                                           obj_id=self.user.pk, region='test')
+        self.assertEqual(0, count)
