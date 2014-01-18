3288c2c773c8eb19ae91d25d1506f2384b436bfd
Keryn Knight <keryn@kerynknight.com>
2014-01-18 15:35:38 +0000

    bugfix: clean not being called, and validation error being broken.

--- editregions/models.py
+++ editregions/models.py
@@ -71,10 +71,11 @@ class EditRegionChunk(ChangeTracking, Generic):
     move.alters_data = True
 
     def clean(self):
+        super(EditRegionChunk, self).clean()
         if self.position is None or self.position == 0:
             self.position = 1
         if self.content_id is None:
-            raise ValidationError("{0.__class__ requires `content_id` to be "
+            raise ValidationError("{0.__class__} requires `content_id` to be "
                                   "the parent object's primary key")
 
     class Meta:
