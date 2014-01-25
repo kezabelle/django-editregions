7604b21c4a21b378b7dd022749d338ed8c5645d6
Keryn Knight <keryn@kerynknight.com>
2014-01-25 19:20:01 +0000

    testing image parsing from the file field. depends on a PNG file taken from the Django project’s unit tests.

--- editregions/tests/contrib/uploads/models.py
+++ editregions/tests/contrib/uploads/models.py
@@ -1,18 +1,22 @@
 # -*- coding: utf-8 -*-
+import os
 from django.contrib.auth.models import User
+from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import TestCase
 from django.utils.encoding import force_text
 from editregions.contrib.uploads.models import File
 from editregions.utils.data import get_content_type
+from django.conf import settings
 
 
 class FileTestCase(TestCase):
     def setUp(self):
+        fakefile = SimpleUploadedFile('x/y/z.gif', 'xyz')
         sample_user, created = User.objects.get_or_create()
         user_ct = get_content_type(sample_user)
         self.file = File(position=1, content_type=user_ct,
                          content_id=sample_user.pk, region='test',
-                         title='x', data='x/y/z.gif')
+                         title='x', data=fakefile)
 
     def test_get_filename(self):
         self.assertEqual('z.gif', self.file.get_filename())
@@ -31,3 +35,33 @@ class FileTestCase(TestCase):
         self.file.data = None
         self.file.title = None
         self.assertEqual('No file or title', force_text(self.file))
+
+    def test_is_not_image(self):
+        self.assertFalse(self.file.is_image())
+
+    def test_get_dimensions_not_image(self):
+        self.assertIsNone(self.file.dimensions)
+
+    def test_get_dimensionsstr_not_image(self):
+        self.assertEqual('', self.file.dimensions_as_str())
+
+    def test_is_image(self):
+        file_ = os.path.join(settings.STATICFILES_DIRS[0], "test.png")
+        with open(file_) as f:
+            fakefile = SimpleUploadedFile('x/y/z.gif', f.read())
+        self.file.data = fakefile
+        self.assertTrue(self.file.is_image())
+
+    def test_get_dimensions_image(self):
+        file_ = os.path.join(settings.STATICFILES_DIRS[0], "test.png")
+        with open(file_) as f:
+            fakefile = SimpleUploadedFile('x/y/z.gif', f.read())
+        self.file.data = fakefile
+        self.assertEqual((16, 16), self.file.dimensions)
+
+    def test_get_dimensionsstr_image(self):
+        file_ = os.path.join(settings.STATICFILES_DIRS[0], "test.png")
+        with open(file_) as f:
+            fakefile = SimpleUploadedFile('x/y/z.gif', f.read())
+        self.file.data = fakefile
+        self.assertEqual('16x16', self.file.dimensions_as_str())
