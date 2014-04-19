1921c600a2c405bcd042dcab247df1785fffdcc6
Keryn Knight <keryn@kerynknight.com>
2014-04-19 13:47:50 +0100

    increase default number of models to fetch together.

--- editregions/constants.py
+++ editregions/constants.py
@@ -4,7 +4,7 @@ REQUEST_VAR_REGION = 'region'
 REQUEST_VAR_CT = 'content_type'
 REQUEST_VAR_ID = 'content_id'
 
-SPLIT_CHUNKS_EVERY = 7
+SPLIT_CHUNKS_EVERY = 14
 
 #: the format of the cache key, to be filled so that storing and deleting
 #: rendered regions can take place.
