b0b7d8b79d60e9e8d602f96983dcd65eb4669c3a
Keryn Knight <keryn@kerynknight.com>
2014-11-01 18:07:13 +0000

    send even the pre-rendered markdown through the template loader system, so that custom templates may be used to wrap the content if desired.

--- editregions/contrib/textfiles/admin.py
+++ editregions/contrib/textfiles/admin.py
@@ -2,6 +2,7 @@
 from django.contrib import admin
 from django.contrib.admin import ModelAdmin
 from django.template.defaultfilters import striptags
+from django.template.loader import render_to_string
 from editregions.admin.modeladmins import ChunkAdmin
 from .models import Markdown
 from .forms import MarkdownSelectionForm
@@ -15,7 +16,8 @@ class MarkdownAdmin(ChunkAdmin, ModelAdmin):
     ]
 
     def render_into_region(self, obj, context, **kwargs):
-        return obj.rendered_content
+        return render_to_string('editregions/textfiles/markdown.html',
+                                context_instance=context)
 
     def render_into_summary(self, obj, context, **kwargs):
         return striptags(obj.rendered_content).strip()
